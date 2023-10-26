db745c46c782f280b0f2163665aa9d4f98490da6
Boyvalenko Nikita <n.boywalenko@yandex.ru>
2023-10-26 22:49:39 +0700

    Add button in the forms

--- js/registration.js
+++ js/registration.js
@@ -3,7 +3,7 @@ function OutEnter() {
     var enter = document.getElementById("enter");
     enter.style.display = "none";
 
-    //Очистка полей после закрытия окна регистрации
+    //Очистка полей после закрытия окна входа
     var login = document.getElementById("login");
     var password = document.getElementById("password");
     login.value = '';
@@ -17,6 +17,14 @@ function InEnter() {
 function OutRegistration() {
     var enter = document.getElementById("registration");
     enter.style.display = "none";
+
+    //Очистка полей после закрытия окна регистрации
+    var login = document.getElementById("loginReg");
+    var password = document.getElementById("passwordReg");
+    var email = document.getElementById("emailReg");
+    login.value = '';
+    password.value = '';
+    email.value = '';
 }
 function InRegistration() {
     var enter = document.getElementById("registration");
@@ -26,6 +34,12 @@ function InRegistration() {
 function OutIForgotPassword() {
     var enter = document.getElementById("iForgotPassword");
     enter.style.display = "none";
+
+    //Очистка полей после закрытия окна регистрации
+    var login = document.getElementById("loginForgot");
+    var email = document.getElementById("emailForgot");
+    login.value = '';
+    email.value = '';
 }
 function InIForgotPassword() {
     var enter = document.getElementById("iForgotPassword");
